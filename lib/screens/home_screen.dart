2ca8a3d1b895d0b3188fcc3f28da5e0b5dc4417a
Omar Abdullah <omarmoharam790@gmail.com>
2024-11-05 00:34:29 +0200

    Custom AppBar Added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,17 +5,68 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Notes Home Page',
-          style: TextStyle(
-            color: Colors.indigo,
-            fontFamily: 'Poppins',
-            fontSize: 32,
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 32,
           ),
-        ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Hey Chris ,\nGood Morning",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                ),
+                maxLines: 2,
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              // Container(
+              //   width: 80,
+              //   height: 80,
+              //   clipBehavior: Clip.hardEdge,
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(25),
+              //   ),
+              //   child: Image.asset(
+              //     // 'assets/images/8de0ff70-e27f-4add-b884-bad03f400865.jpg',
+              //     'assets/images/profile.png',
+              //     width: 80,
+              //     height: 80,
+              //   ),
+              // ),
+              Container(
+                  width: 90,
+                  height: 90,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(35),
+                    // border: Border.all(
+                    //   width: 2,
+                    // ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      'assets/images/8de0ff70-e27f-4add-b884-bad03f400865.jpg',
+                      // 'assets/images/profile.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ))
+              // const Icon(
+              //   Icons.notifications_none_outlined,
+              //   size: 30,
+              // ),
+            ],
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
