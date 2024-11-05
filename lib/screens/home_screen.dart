14d5f5c78fb2fd80049f7680d10a78c3a035cd37
Omar Abdullah <omarmoharam790@gmail.com>
2024-11-05 20:24:03 +0200

    Custom Search bar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,16 +7,58 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Padding(
-      padding: EdgeInsets.all(12.0),
+      padding: const EdgeInsets.all(12.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SizedBox(
+          const SizedBox(
             height: 32,
           ),
-          CustomAppBar(),
+          const CustomAppBar(),
+          const SizedBox(height: 25),
+          Container(
+            height: 46,
+            width: 335,
+            decoration: const BoxDecoration(
+              color: Color(0xFFE6E6E6),
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 12.0),
+                    child: Icon(Icons.search),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 260,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        fillColor: Color(0xFFE6E6E6),
+                        label: Text('Search'),
+                        labelStyle: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'Poppins',
+                          color: Colors.black,
+                        ),
+                        floatingLabelAlignment: FloatingLabelAlignment.start,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     ));
