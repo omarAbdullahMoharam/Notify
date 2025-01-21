1221322984b970967cc3fdda4f6adcf440a6f99e
Omar Abdullah <omarmoharam790@gmail.com>
2025-01-21 03:32:14 +0200

    NoteviewCubit added

--- lib/Controller/cubits/notes_view_cubit/notes_view_cubit.dart
+++ lib/Controller/cubits/notes_view_cubit/notes_view_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-part 'notes_cubit_state.dart';
+part 'notes_view_state.dart';
 
 class NotesCubitCubit extends Cubit<NotesCubitState> {
   NotesCubitCubit() : super(NotesCubitInitial());
